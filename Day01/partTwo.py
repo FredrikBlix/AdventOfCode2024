090e2829b548c25d73e1d004770516f75b806e07
Fredrik Blix <fredrik.blix@sogeti.com>
2024-12-01 07:02:06 +0100

    Day01 first commit

---
+++ Day01/partTwo.py
@@ -0,0 +1,36 @@
+import re
+
+def parse_input(input_data, verbose = False):
+    left_list = []
+    right_list = []
+    row_index = 0
+    input_data = input_data.splitlines()
+
+    for row in input_data:
+        row_index += 1
+        numbers = re.findall(r'\d+', row)
+
+        if verbose:
+            print('row [', row_index, '] = ', row, numbers)
+
+        if numbers and len(numbers) == 2:
+            left_list.append(int(numbers[0]))
+            right_list.append(int(numbers[1]))
+            
+    left_list.sort()
+    right_list.sort()
+
+    return left_list, right_list
+
+def distance_between_points(x, y):
+    return abs(x - y)
+
+def distance_between_lists(left_list, right_list):
+    total_sum = 0
+    for i in range(len(left_list)):
+        total_sum +=  distance_between_points(left_list[i], right_list[i])
+    return total_sum
+
+def part_two(input_data, verbose = False):
+    left_list, right_list = parse_input(input_data, verbose)
+    return distance_between_lists(left_list, right_list)
---
+++ Day01/test/test_one.py
@@ -0,0 +1,30 @@
+import unittest
+import os
+
+from partOne import part_one, parse_input, distance_between_points, distance_between_lists
+
+class TestMainFunctions(unittest.TestCase):
+
+    def setUp(self):
+        base_path = os.path.dirname(__file__)
+        with open(os.path.join(base_path, 'input_sample_one.txt'), 'r') as file:
+            self.input_sample_one = file.read()
+
+    def test_part_one(self):
+        result = part_one(self.input_sample_one.strip())
+        self.assertEqual(result, 11)
+    
+    def test_parse_input(self):
+        result = parse_input(self.input_sample_one.strip())
+        self.assertEqual(result, ([1, 2, 3, 3, 3, 4], [3, 3, 3, 4, 5, 9]))
+        
+    def test_destance_between_points(self):
+        result = distance_between_points(1, 2)
+        self.assertEqual(result, 1)
+    
+    def test_destance_between_lists(self):
+        result = distance_between_lists([1, 2, 3, 3, 3, 4], [3, 3, 3, 4, 5, 9])
+        self.assertEqual(result, 11)
+
+if __name__ == '__main__':
+    unittest.main()
