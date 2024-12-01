146667cf98a96a348e4611622af7887b019409f2
Fredrik Blix <fredrik.blix@sogeti.com>
2024-12-01 07:23:15 +0100

    Refactor partTwo.py to improve similarity scoring and update tests accordingly

--- Day01/partTwo.py
+++ Day01/partTwo.py
@@ -10,27 +10,24 @@ def parse_input(input_data, verbose = False):
         row_index += 1
         numbers = re.findall(r'\d+', row)
 
-        if verbose:
-            print('row [', row_index, '] = ', row, numbers)
-
         if numbers and len(numbers) == 2:
             left_list.append(int(numbers[0]))
             right_list.append(int(numbers[1]))
             
     left_list.sort()
     right_list.sort()
-
+            
     return left_list, right_list
 
-def distance_between_points(x, y):
-    return abs(x - y)
+def find_number_matches(x, list):
+    return list.count(x)
 
-def distance_between_lists(left_list, right_list):
-    total_sum = 0
+def similarity_score(left_list, right_list):
+    score = 0
     for i in range(len(left_list)):
-        total_sum +=  distance_between_points(left_list[i], right_list[i])
-    return total_sum
+        score += left_list[i] * find_number_matches(left_list[i], right_list)
+    return score
 
 def part_two(input_data, verbose = False):
     left_list, right_list = parse_input(input_data, verbose)
-    return distance_between_lists(left_list, right_list)
+    return similarity_score(left_list, right_list)
--- Day01/test/test_two.py
+++ Day01/test/test_two.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 
-from partOne import part_one, parse_input, distance_between_points, distance_between_lists
+from partTwo import part_two, parse_input, find_number_matches, similarity_score
 
 class TestMainFunctions(unittest.TestCase):
 
@@ -10,21 +10,21 @@ class TestMainFunctions(unittest.TestCase):
         with open(os.path.join(base_path, 'input_sample_two.txt'), 'r') as file:
             self.input_sample_one = file.read()
 
-    def test_part_one(self):
-        result = part_one(self.input_sample_one.strip())
-        self.assertEqual(result, 11)
+    def test_part_two(self):
+        result = part_two(self.input_sample_one.strip())
+        self.assertEqual(result, 31)
     
     def test_parse_input(self):
         result = parse_input(self.input_sample_one.strip())
         self.assertEqual(result, ([1, 2, 3, 3, 3, 4], [3, 3, 3, 4, 5, 9]))
         
-    def test_destance_between_points(self):
-        result = distance_between_points(1, 2)
+    def test_find_number_matches(self):
+        result = find_number_matches(1, [1,2,3,4,5])
         self.assertEqual(result, 1)
     
-    def test_destance_between_lists(self):
-        result = distance_between_lists([1, 2, 3, 3, 3, 4], [3, 3, 3, 4, 5, 9])
-        self.assertEqual(result, 11)
+    def test_similarity_score(self):
+        result = similarity_score([1, 2, 3, 3, 3, 4], [3, 3, 3, 4, 5, 9])
+        self.assertEqual(result, 31)
 
 if __name__ == '__main__':
     unittest.main()
