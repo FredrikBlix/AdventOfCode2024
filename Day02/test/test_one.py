73eb94fc63b6efba59c78f0e0638cd386cdacc63
Fredrik Blix <fredrik.blix@sogeti.com>
2024-12-02 11:42:23 +0100

    Refactor test cases for is_it_safe function to consolidate tests and improve clarity

--- Day02/test/test_one.py
+++ Day02/test/test_one.py
@@ -31,27 +31,17 @@ class TestMainFunctions(unittest.TestCase):
         result = max_difference([1, 2, 3, 4, 5])
         self.assertEqual(result, True)
     
-    def test_is_it_safe_1(self):
+    def test_is_it_safe(self):
         result = is_it_safe([7, 6, 4, 2, 1])
         self.assertEqual(result, True)
-
-    def test_is_it_safe_2(self):
         result = is_it_safe([1, 2, 7, 8, 9])
         self.assertEqual(result, False)
-
-    def test_is_it_safe_3(self):
         result = is_it_safe([9, 7, 6, 2, 1])
         self.assertEqual(result, False)
-
-    def test_is_it_safe_4(self):
         result = is_it_safe([1, 3, 2, 4, 5])
         self.assertEqual(result, False)
-
-    def test_is_it_safe_5(self):
         result = is_it_safe([8, 6, 4, 4, 1])
         self.assertEqual(result, False)
-
-    def test_is_it_safe_6(self):
         result = is_it_safe([1, 3, 6, 7, 9])
         self.assertEqual(result, True)
 
